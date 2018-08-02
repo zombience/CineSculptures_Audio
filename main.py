83a2eb42bd685f3275ab29d1a2ef2701ae767fe4
darksatellite <jason@iedlabs.net>
2018-08-02 17:47:25 -0500

    beginning test cases

--- main.py
+++ main.py
@@ -1,31 +1,34 @@
 import spectro
 import writeobj
 import audioanalysis
-import tkinter as tk
-from tkinter import filedialog
+#import tkinter as tk
+#from tkinter import filedialog
 import audioscape
+import argparse
 
-def main():
-
+def main(args):
+    print(args.file)
     # set a default input file
-    infile = ""
+    infile = args.file
 
     # setup dialog window
-    root = tk.Tk()
-    root.withdraw()
-    root.update()
+    #root = tk.Tk()
+    #root.withdraw()
+    #root.update()
 
     # get audio file
-    while (infile[-3:] != "wav"):
-        infile = filedialog.askopenfilename()
+    #while (infile[-3:] != "wav"):
+    #    infile = filedialog.askopenfilename()
 
-    root.quit()
+    #root.quit()
 
-    chunk = 4020 # three seconds is 3 * 44100, but it doesn't look good.
+    chunk = args.chunk#4020 # three seconds is 3 * 44100, but it doesn't look good.
+
+
 
     # create AudioScape object
-    ascape = audioscape.AudioScape(infile, chunk)
-    print("Audio File:", infile)
+    ascape = audioscape.AudioScape(infile, int(chunk))
+    print("running analysis on audio File:", infile, 'with chunksize: ', chunk)
 
     ascape.analyze()
     ascape.trim(14000) # the film audio seems to by lowpassed at 14000, so I trimmed everything above that
@@ -38,4 +41,8 @@ def main():
     ascape.close()
     return 0
 
-main()
+parser = argparse.ArgumentParser(description='CineSculptures arguments')
+parser.add_argument('-file', metavar='file', required=True, help='file to read: must be .wav for now')
+parser.add_argument('-chunk', metavar='chunksize', default=4020, help='fft process size: larger sizez = higher frequency precision, lower time domain accuracy (i.e. time smearing)')
+args = parser.parse_args()
+main(args)
---
+++ test/test.py
@@ -0,0 +1,15 @@
+
+import sys
+import argparse
+
+
+def main(args):
+	print('python ran with args: ', args.file)
+
+#main(sys.argv)
+
+parser = argparse.ArgumentParser(description='CineSculptures arguments')
+parser.add_argument('-file', metavar='file', required=True, help='file to read: must be .wav for now')
+parser.add_argument('-chunk', metavar='chunksize', default=4020, help='fft process size: larger sizez = higher frequency precision, lower time domain accuracy (i.e. time smearing)')
+args = parser.parse_args()
+main(args)
